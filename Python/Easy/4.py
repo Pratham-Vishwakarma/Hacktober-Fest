5b0394b2e0fec42f3697ba54bf4abc49a5b0141d
Pratham-Vishwakarma <147902380+Pratham-Vishwakarma@users.noreply.github.com>
2024-10-05 12:22:09 +0530

    Update 4.py
    Blaze:
    Python

--- Python/Easy/4.py
+++ Python/Easy/4.py
@@ -1,9 +1,9 @@
-message = ‘py" 
+message = "py" 
 print(message)
 
-title = "Python's "cool" feature"
-
+title = "Python's cool feature"
+print(title)
 description = 'This is a Python "error example'
-
-sentence = “Hello, Python!”
+print(description)
+sentence = "Hello, Python!"
 print(sentence)
